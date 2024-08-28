029c61ea67a912d5d876e73df2a211eef6f98476
IT24103083 <it24103083@my.sliit.lk>
2024-08-28 09:39:56 +0530

    Add files via upload

---
+++ Q1/IT24103083Lab6Q1.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+   public class IT24103083Lab6Q1{
+     public static void main (String[] args) {
+   
+   
+     Scanner  sc= new Scanner (System.in);
+    
+     System.out.print("Enter a number:");
+     int number   = sc.nextInt();
+     
+     double squre =number *number;
+     double squreroot=Math.sqrt(number);
+  
+     System.out.println("The squre of "+number +"is: "+squre);
+     System.out.println(" The squre root of "+number + "is:"+squreroot);
+ }
+}
+     
+ 
+    
+     
+     
+     
+     
+   
