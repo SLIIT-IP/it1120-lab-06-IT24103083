849960c1b09d38a481d64d33f64615461687910e
IT24103083 <it24103083@my.sliit.lk>
2024-09-02 22:21:05 +0530

    Add files via upload

---
+++ Q2/IT24103083Lab6Q2A.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+ public class IT24103083Lab6Q2A
+ {
+    public static void main(String[] args){
+    int number=1;
+    while(number <=10)
+    {
+       System.out.println(number);
+       number=number +1;
+    }
+  }
+}
